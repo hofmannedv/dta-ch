011a5998d03c401a2daf112854ebbf802156bb20
Frank Hofmann <frank.hofmann@efho.de>
2015-07-30 12:20:29 +0200

    php test: added copyright notice

--- php-test/test.php
+++ php-test/test.php
@@ -1,3 +1,15 @@
 <?php
+
+/*
+-----------------------------------------------------------
+display info about the PHP modules that are installed, currently
+
+(C) 2015 Frank Hofmann, Berlin, Germany
+Released under GNU Public License (GPL)
+email frank.hofmann@efho.de
+-----------------------------------------------------------
+*/
+
+// call internal PHP function
 phpinfo();
 ?>
