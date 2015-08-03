50972d1e6ba94f276c5c687abdd07db759457779
Frank Hofmann <frank.hofmann@efho.de>
2015-08-03 15:00:41 +0200

    example: example to read csv data -- process a single transaction, and multiple transactions

--- example/example-csv.php
+++ example/example-csv.php
@@ -10,4 +10,54 @@ email frank.hofmann@efho.de
 -----------------------------------------------------------
 */
 
+// include dta-ch object class for payments
+require_once 'dta-ch.php';
+
+function processSingleTransaction ($transactionData) {
+	// process a single transaction
+
+	// create dta-ch object
+	// initialize a new dta-ch object
+	$dta = new DTACH();
+
+	// fill object with data
+	// - set data format: fixed
+	$dtaDataFormat = "fixed";
+	$dta->setDataFormat($dtaDataFormat);
+
+	// - set date of delivery: today
+	$dateOfDelivery = date("ymd");
+	$dta->setDateOfDelivery($dateOfDelivery);
+
+	// - import transaction data as csv data
+	$importValue = $dta->importCsv($transactionData);
+
+	// - auto-adjust data: header, and data fields
+	$dta->adjustHeader();
+	$dta->adjustDataFields();
+
+	// - validate data: header, and data fields
+	$dta->validateHeader();
+	$dta->validateDataFields();
+
+	return $dta;
+}
+
+function processMultipleTransaction ($transactionData) {
+	// process multiple transactions
+
+	// define result list
+	$dtaList = array();
+
+	// go through the list of transactions one by one
+	foreach ($transactionData as $singleTransaction) {
+		$dta = processSingleTransaction ($singleTransaction);
+
+		// add new dta to dta list
+		$dtaList[] = $dta;
+
+	// return list of transactions
+	return $dtaList;
+}
+
 ?>
