b3ced341a58206992f9c6affbd03538f2d16e56e
Frank Hofmann <frank.hofmann@efho.de>
2015-08-03 14:37:36 +0200

    example: example to read csv data -- initial commit

---
+++ example/example-csv.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+-----------------------------------------------------------
+DTA-CH example reading CSV data
+
+(C) 2015 Frank Hofmann, Berlin, Germany
+Released under GNU Public License (GPL)
+email frank.hofmann@efho.de
+-----------------------------------------------------------
+*/
+
+?>
