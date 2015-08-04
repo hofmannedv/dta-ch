929863cf9a4ddedb4e6c3b91473390d84282c00e
Frank Hofmann <frank.hofmann@efho.de>
2015-08-04 16:06:49 +0200

    example: example to read csv data -- update the transaction ids

--- example/example-csv.php
+++ example/example-csv.php
@@ -266,6 +266,24 @@ $ta890 = createTA890 ($dataFormat, $dataFileSenderIdentification, $totalValue);
 // add the TA 890 record to the transactionList
 $sortedDtaList[] = $ta890;
 
+// adjust numbering of the single dta records
+$nr = 1;
+foreach ($sortedDtaList as $key => $dta) {
+	// convert the integer value into string, and extend it by 
+	// leading zeros
+	$number = strval($nr);
+	$number = str_pad($number, 5, "0", STR_PAD_LEFT);
+
+	// set the sequence number of the current dta entry
+	$dta->setEntrySequenceNumber($number);
+
+	// update the transaction value
+	$sortedDtaList[$key] = $dta;
+
+	// increase the number by one
+	$nr++;
+}
+
 // --------------------------------------------
 
 ?>
