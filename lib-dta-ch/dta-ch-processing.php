850a3b7f7dc40d9ff32cdd02d29f17be99ca1123
Frank Hofmann <frank.hofmann@efho.de>
2015-08-05 13:33:23 +0200

    lib-dta-ch: DTACHProcessing class to prepare single transactions

---
+++ lib-dta-ch/dta-ch-processing.php
@@ -0,0 +1,18 @@
+<?php
+/*
+-----------------------------------------------------------
+DTA-CH support class to process single transactions
+Requires lib-dta-ch.
+
+(C) 2015 Frank Hofmann, Berlin, Germany
+Released under GNU Public License (GPL)
+email frank.hofmann@efho.de
+-----------------------------------------------------------
+*/
+
+class DTACHProcessing {
+	function __construct() {
+
+	}
+}
+?>
